9596c993d654ea259bfc6ca472cfa6b93340bf16
Emil Mirzazada <mirzazadeemil2005@gmail.com>
2023-11-09 23:39:11 +0400

    Added methods to TasksController class, isDone field to the Task class and partially implemented makeDone and makeUnDone methods in IncomeTask and ExpenseTask classes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.clear();
 
 
 function Task(description, cost) {
-    if (new.target !== IncomeTask && new.target !==ExpenseTask) {
+    if (new.target !== IncomeTask && new.target !== ExpenseTask) {
         throw new Error("Function constructor Task cannot be invoked with 'new'");
     }
     if (cost < 0) {
@@ -13,6 +13,7 @@ function Task(description, cost) {
     const _id = "id" + Math.random().toString(16).slice(2);
     const _description = description;
     const _cost = cost;
+    let _isDone=false;
 
     Object.defineProperties(this, {
         id: {
@@ -29,37 +30,50 @@ function Task(description, cost) {
             get() {
                 return _cost;
             }
+        },
+        isDone:{
+            get(){
+                return _isDone;
+            },
+            set(isDone){
+                _isDone=isDone;
+            }
         }
     })
 }
 
 class IncomeTask extends Task {
-    
+
     constructor(description, cost) {
         super(description, cost);
     }
 
     makeDone(budget) {
+        this.isDone = true;
         // budget.income + cost
     }
 
     makeUnDone(budget) {
+        this.isDone = false;
         // budget.income - cost
     }
-    
+
 }
 
 class ExpenseTask extends Task {
-    
+
     constructor(description, cost) {
         super(description, cost);
     }
 
+
     makeDone(budget) {
+        this.isDone = true;
         // budget.expenses + cost
     }
 
     makeUnDone(budget) {
+        this.isDone = false;
         // budget.expenses - cost
     }
 }
@@ -68,13 +82,107 @@ class TasksController {
     #tasks;
 
     constructor(tasks) {
-        this.#tasks = tasks;
+        this.#tasks = [...tasks];
     }
 
-    get tasks(){
-        return this.#tasks;
+    addTasks(...tasks) {
+        for (let task of tasks) {
+            let boolArr = this.#tasks.map(x => x.id === task.id ? false : true);
+            if (!boolArr.includes(false)) {
+                this.#tasks.push(task);
+            }
+        }
     }
 
+    getTasks() {
+        return [...this.#tasks];
+    }
 
+    getTasksSortedBy(string) {
+        let arr = [...this.#tasks];
+        if (string === 'description') {
+            arr.sort(function (a, b) {
+                if (a.description > b.description) {
+                    return 1;
+                }
+                if (a.description < b.description) {
+                    return -1;
+                }
+                return 0;
+            });
+        }
+        else if (string === 'cost') {
+            arr.sort(function (a, b) {
+                return a.cost - b.cost;
+            });
+        }
+        else if (string === 'status') {
+            arr.sort(function (a, b) {
+                if (b.isDone === a.isDone) {
+                    return 0;
+                }
+                if (b.isDone) {
+                    return 1;
+                }
+                if (a.isDone) {
+                    return -1;
+                }
+            });
+        }
+        return arr;
+    }
+
+    getFilteredTasks(obj) {
+        let arr = [...this.#tasks];
+        let tempArr = [...this.#tasks];
+
+        if (obj.description !== undefined) {
+            for (let task of arr) {
+                if (task.description !== obj.description) {
+                    tempArr.splice(tempArr.indexOf(task), 1);
+                }
+            }
+            arr = [...tempArr];
+        }
+        if (obj.isIncome !== undefined) {
+            if (obj.isIncome) {
+                for (let task of arr) {
+                    let index = tempArr.indexOf(task);
+                    if (!task instanceof IncomeTask) {
+                        tempArr.splice(index, 1);
+                    }
+                }
+            }
+            else {
+                for (let task of arr) {
+                    let index = tempArr.indexOf(task);
+                    if (!task instanceof ExpenseTask) {
+                        tempArr.splice(index, 1);
+                    }
+                }
+            }
+            arr = [...tempArr];
+        }
+        if (obj.isCompleted !== undefined) {
+            if (obj.isCompleted) {
+                for (let task of arr) {
+                    let index = tempArr.indexOf(task);
+                    if (!task.isDone) {
+                        tempArr.splice(index, 1);
+                    }
+                }
+            }
+            else {
+                for (let task of arr) {
+                    let index = tempArr.indexOf(task);
+                    if (task.isDone) {
+                        tempArr.splice(index, 1);
+                    }
+                }
+            }
+            arr = [...tempArr];
+        }
+        return arr;
+    }
 }
 
